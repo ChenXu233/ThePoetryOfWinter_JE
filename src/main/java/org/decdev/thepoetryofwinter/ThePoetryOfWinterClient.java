9f2c2efb5682d290fc19fee1085186a9e76ba978
XuChenXu <91937041+ChenXu233@users.noreply.github.com>
2024-10-20 14:49:08 +0800

    :rocket: 按教程重构

---
+++ src/main/java/org/decdev/thepoetryofwinter/ThePoetryOfWinterClient.java
@@ -0,0 +1,11 @@
+package org.decdev.thepoetryofwinter;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class ThePoetryOfWinterClient implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
---
+++ src/main/java/org/decdev/thepoetryofwinter/ThePoetryOfWinterDataGenerator.java
@@ -0,0 +1,11 @@
+package org.decdev.thepoetryofwinter;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class ThePoetryOfWinterDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
